296e942aacbb37bc55a4a366acff464980ce642d
Ketuojo <sirketxp@gmail.com>
2025-01-06 15:44:04 +0100

    push

---
+++ src/components/misc/Breadcrumb.jsx
@@ -0,0 +1,9 @@
+const Breadcrumb = () => {
+  return (
+    <div>
+      <p>Breadcrumb here...</p>
+    </div>
+  );
+};
+
+export default Breadcrumb;
---
+++ src/components/modals/Modal.jsx
@@ -0,0 +1,9 @@
+const Modal = () => {
+  return (
+    <div>
+      <p>Modal here...</p>
+    </div>
+  );
+};
+
+export default Modal;
